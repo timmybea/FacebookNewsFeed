3fb3a0c48f13dd8479c2fc314b312d581142b4e8
Tim Beals <tim.beals@gmail.com>
2017-02-21 11:32:50 -0800

    Basic setup of window and root controller

---
+++ FacebookNewsFeed/NewsFeedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NewsFeedViewController.swift
+//  FacebookNewsFeed
+//
+//  Created by Tim Beals on 2017-02-21.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "News Feed"
+        self.view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
